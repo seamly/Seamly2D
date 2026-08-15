1e0c2c48d6a625d792f119f72f6e3a40f3838ea4
slspencer <susan.spencer@gmail.com>
2026-08-15 15:23:55 +0000

    Deploying to gh-pages from @ seamly/Seamly2D@2c4ca264325f6a1d2d2b7deb6c99e445819466c1 🚀

--- docs/search/all_5.js
+++ docs/search/all_5.js
@@ -347,7 +347,7 @@ var searchData=
   ['eps_344',['EPS',['../d4/da4/def_8h.html#a1f9b518e0e2d2c25a541f09e2e49a9d8ac2c027d8c62500300145c3043546d4c6',1,'def.h']]],
   ['epsilon_345',['epsilon',['../d6/d2f/predicates_8cpp.html#a294322d88f49e20954d824785a25bd91',1,'predicates.cpp']]],
   ['epsilon_346',['EPSILON',['../de/d57/delaunay_8cpp.html#a002b2f4894492820fe708b1b7e7c5e70',1,'delaunay.cpp']]],
-  ['epsilonnode_347',['epsilonNode',['../d4/d39/classCMNode.html#ad1dedbde6035c5de088ab58f85a1bb3ba2b441e196cde066ae81f983a55eed57d',1,'CMNode']]],
+  ['epsilonnode_347',['epsilonNode',['../d4/d39/classCMNode.html#ad0b4e32b57382535a3610d03e746926ea2b441e196cde066ae81f983a55eed57d',1,'CMNode']]],
   ['eqn_5ftest_5fbulk_348',['EQN_TEST_BULK',['../df/dbf/qmuparsertest_8cpp.html#a95d5533a83afb20e4994d936f88ca43c',1,'qmuparsertest.cpp']]],
   ['eqntest_349',['EqnTest',['../da/d59/classqmu_1_1Test_1_1QmuParserTester.html#a4bdae81e2caabfb15e3b16082792a3d0',1,'qmu::Test::QmuParserTester']]],
   ['eqntestbulk_350',['EqnTestBulk',['../da/d59/classqmu_1_1Test_1_1QmuParserTester.html#aff2822768ab9ca09a99effdd7901d60d',1,'qmu::Test::QmuParserTester']]],
--- docs/search/enumvalues_4.js
+++ docs/search/enumvalues_4.js
@@ -157,7 +157,7 @@ var searchData=
   ['enum_5fnomoreelements_154',['Enum_NoMoreElements',['../dc/d80/classXMLExcepts.html#a0b13780541d8e00bfae96459fece2e80a334cacc7cfcef4078f4d4f6bf327a0f1',1,'XMLExcepts']]],
   ['enumeration_155',['Enumeration',['../df/d1f/classXMLAttDef.html#ad029f037065bfaeb991ab7855de79255a8ae2d3acb26c09adbc2545467a8b1325',1,'XMLAttDef']]],
   ['eps_156',['EPS',['../d4/da4/def_8h.html#a1f9b518e0e2d2c25a541f09e2e49a9d8ac2c027d8c62500300145c3043546d4c6',1,'def.h']]],
-  ['epsilonnode_157',['epsilonNode',['../d4/d39/classCMNode.html#ad1dedbde6035c5de088ab58f85a1bb3ba2b441e196cde066ae81f983a55eed57d',1,'CMNode']]],
+  ['epsilonnode_157',['epsilonNode',['../d4/d39/classCMNode.html#ad0b4e32b57382535a3610d03e746926ea2b441e196cde066ae81f983a55eed57d',1,'CMNode']]],
   ['equal_158',['EQUAL',['../d5/d4b/classXMLNumber.html#aacbe31656907b1e071997df9fae7bbebad89d8d5ea15d0d5e8d58e26f81adddc3',1,'XMLNumber']]],
   ['error_159',['Error',['../da/d7d/classVPieceItem.html#a09ecb9f194c9e7bb7b1008938050cd03a8e79a2da272950adbbced9e206d843a8',1,'VPieceItem']]],
   ['errorinstall_160',['ErrorInstall',['../df/d78/abstracttest_8h.html#afedab7240f96815528593a589a88bc28a707e2b530cdbf1cb84fdf292ebae20b6',1,'abstracttest.h']]],
