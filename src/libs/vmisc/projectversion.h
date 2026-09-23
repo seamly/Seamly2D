c7a427bdfed35c2937a8e71326bda3945bde99b5
slspencer <susan.spencer@gmail.com>
2026-09-23 13:40:56 -0500

    fixed selecting small pieces in Adjust mode

--- src/libs/vmisc/projectversion.h
+++ src/libs/vmisc/projectversion.h
@@ -79,8 +79,8 @@ extern const QString APP_VERSION_STR;
 #define WINDOW_STATE_VERSION 1
 
 // Start: Do not edit here, use packaging/version.sh to update
-#define VER_FILEVERSION 26,9,22,1381
-#define VER_FILEVERSION_STR "26.9.22.1381"
+#define VER_FILEVERSION 26,9,23,609
+#define VER_FILEVERSION_STR "26.9.23.609"
 // End: Do not edit here
 
 #define V_PRERELEASE // Mark prerelease builds
